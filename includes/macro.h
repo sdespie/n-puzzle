3104b03e8f0e5edbb43f66b070eb5b1fe45e9f16
Alexis Defontaine <adefonta@e-r6-p1.s19.be>
2019-04-11 19:31:01 +0200

    Correct level max hash

--- includes/macro.h
+++ includes/macro.h
@@ -6,7 +6,7 @@
 /*   By: adefonta <adefonta@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/01 20:11:25 by adefonta          #+#    #+#             */
-/*   Updated: 2019/04/11 19:26:13 by adefonta         ###   ########.fr       */
+/*   Updated: 2019/04/11 19:30:42 by adefonta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,6 @@
 # define MAX_MOVES 4
 
 # define HASH_SIZE 500
-# define HASH_MAX_LEVEL 10
+# define HASH_MAX_LEVEL 5
 
 #endif
